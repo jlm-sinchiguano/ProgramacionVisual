93bf6ae447d0467437566be9814376253524f5b5
Jenny Sinchiguano <67289650+jlm-sinchiguano@users.noreply.github.com>
2021-06-21 11:23:26 -0500

    Update Program.cs

--- Deber1/Program.cs
+++ Deber1/Program.cs
@@ -2,101 +2,92 @@
 
 namespace Deber1
 {
-    
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[]args)
         {
-            var luffy = new OnePiece();
-            var light = new DeathNote();
-            var sakura = new SakuraCardCaptor();
-            var residentEvil2 = new ResidentEvil();
-            var superMarioBros3 = new SuperMarioBros();
-            while (true)
-            {
-                Console.WriteLine("Bienvenido \n Elija una opcion para ver la Información de: \n 1.Animes \n 2.Videojuegos \n 3.Salir");
-                var opcion = Console.ReadLine();
-                int opcion1 = int.Parse(opcion);
-                if (opcion1 == 1)
-                {
-                    Console.WriteLine("--- Anime 1 ---");
+            Console.WriteLine("Bienvenido Se le presentara Informacion sobre Anime y Videojuegos \n");
+            
+            Console.WriteLine ("--- Anime 1 ---" \n);
+            
+            OnePiece luffy = new OnePiece ();
+            
+            luffy.reflejarNombreAnime ();
+            luffy.reflejarAnoInicioEmision ();
+            luffy.reflejarCreadorDelAnime ();
+            luffy.reflejarNombrePersonaje ();
+            luffy.reflejarObjetivo ();
+            luffy.reflejarNombreBanda ();
+            luffy.reflejarTotalIntegrantes ();
+            luffy.reflejarNombresIntegrantesTripulacion ();
+            luffy.reflejarQueEsFrutaDelDiablo ();
+            luffy.reflejarFrutaDelDiablo ();
 
-                    luffy.reflejarNombreAnime();
-                    luffy.reflejarAnoInicioEmision();
-                    luffy.reflejarCreadorDelAnime();
-                    luffy.reflejarNombrePersonaje();
-                    luffy.reflejarObjetivo();
-                    luffy.reflejarNombreBanda();
-                    luffy.reflejarTotalIntegrantes();
-                    luffy.reflejarNombresIntegrantesTripulacion();
-                    luffy.reflejarQueEsFrutaDelDiablo();
-                    luffy.reflejarFrutaDelDiablo();
+            Console.WriteLine ("--- Anime 2 ---" \n);
 
-                    Console.WriteLine ("--- Anime 2 ---");
+            DeathNote light = new DeathNote ();
+            
+            light.reflejarNombreAnime ();
+            light.reflejarAnoInicioEmision ();
+            light.reflejarCreadorDelAnime ();
+            light.reflejarNombrePersonaje ();
+            light.reflejaralias ();
+            light.reflejarObjetivo ();
+            light.reflejarNombreRival ();
+            light.reflejarAliados ();
+            light.reflejarQueEsShinigami ();
+            light.reflejarNombreShinigami ();
 
-                    light.reflejarNombreAnime();
-                    light.reflejarAnoInicioEmision();
-                    light.reflejarCreadorDelAnime();
-                    light.reflejarNombrePersonaje();
-                    light.reflejaralias();
-                    light.reflejarObjetivo();
-                    light.reflejarNombreRival();
-                    light.reflejarAliados();
-                    light.reflejarQueEsShinigami();
-                    light.reflejarNombreShinigami();
+            Console.WriteLine ("--- Anime 3 ---" \n);
 
-                    Console.WriteLine("--- Anime 3 ---");
+            SakuraCardCaptor sakura = new SakuraCardCaptor ();
+            
+            sakura.reflejarNombreAnime ();
+            sakura.reflejarAnoInicioEmision ();
+            sakura.reflejarCreadorDelAnime ();
+            sakura.reflejarGenero ();
+            sakura.reflejarNombrePersonaje ();
+            sakura.reflejarObjetivo ();
+            sakura.reflejarTotalCartas ();
+            sakura.reflejarNombresGuardianes ();
+            sakura.reflejarCreadorCartasGuardianes ();
+            sakura.reflejarAliados ();
 
-                    sakura.reflejarNombreAnime();
-                    salura.reflejarAnoInicioEmision();
-                    sakura.reflejarCreadorDelAnime();
-                    sakura.reflejarGenero();
-                    sakura.reflejarNombrePersonaje();
-                    sakura.reflejarObjetivo();
-                    sakura.reflejarTotalCartas();
-                    sakura.reflejarNombresGuardianes();
-                    sakura.reflejarCreadorCartasGuardianes();
-                    sakura.reflejarAliados();
 
-                }
-                if (opcion1 == 2)
-                {
-                    Console.WriteLine("--- Videojuego 1 ---")
+            Console.WriteLine ("--- Videojuego 1 ---" \n);
+      
+            ResidentEvil residentEvil2 = new ResidentEvil ();
+            
+            residentEvil2.reflejarNombreJuego ();
+            residentEvil2.reflejarEmpresa ();
+            residentEvil2.reflejarGeneroJuego ();
+            residentEvil2.reflejarTotalJuegos ();
+            residentEvil2.reflejarPersonajes ();
+            residentEvil2.reflejarEnemigos ();
+            residentEvil2.reflejarObjetivo ();
+            residentEvil2.reflejarArmamento ();
+            residentEvil2.reflejarCurasVacunas ();
+            residentEvil2.reflejarNumeroEscenarios ();
 
-                    residentEvil2.reflejarNombreJuego();
-                    residentEvil2.reflejarEmpresa();
-                    residentEvil2.reflejarGeneroJuego();
-                    residentEvil2.reflejarTotalJuegos();
-                    residentEvil2.reflejarPersonajes();
-                    residentEvil2.reflejarEnemigos();
-                    residentEvil2.reflejarObjetivo();
-                    residentEvil2.reflejarArmamento();
-                    residentEvil2.reflejarCurasVacunas();
-                    residentEvil2.reflejarNumeroEscenarios();
+            Console.WriteLine ("--- Videojuego 2 ---" \n);
+      
+            SuperMarioBros superMarioBros3 = new SuperMarioBros();
+            
+            superMarioBros3.reflejarNombreJuego ();
+            superMarioBros3.reflejarEmpresa ();
+            superMarioBros3.reflejarGeneroJuego ();
+            superMarioBros3.reflejarTotalJuegos ();
+            superMarioBros3.reflejarPersonaje ();
+            superMarioBros3.reflejarEnemigos ();
+            superMarioBros3.reflejarObjetivo ();
+            superMarioBros3.reflejarPoderes ();
+            superMarioBros3.reflejarCuras ();
+            superMarioBros3.reflejarNumeroEscenarios ();
+      
+            Console.ReadKey();
 
-                    Console.WriteLine("--- Videojuego 2 ---")
+  }
 
-                    superMarioBros3.reflejarNombreJuego();
-                    superMarioBros3.reflejarEmpresa();
-                    superMarioBros3.reflejarGeneroJuego();
-                    superMarioBros3.reflejarTotalJuegos();
-                    superMarioBros3.reflejarPersonaje();
-                    superMarioBros3.reflejarEnemigos();
-                    superMarioBros3.reflejarObjetivo();
-                    superMarioBros3.reflejarPoderes();
-                    superMarioBros3.reflejarCuras();
-                    superMarioBros3.reflejarNumeroEscenarios();
-                }
-                
-                }
-                else
-                {
-                    Console.Write("Saliendo-----");
-                    break;
-                }
-                Console.ReadKey();
-            }
-
-        }
-    }
+}
+   
 }
